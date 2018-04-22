b7b87eb45f0eb43ddaca93ad3e35bf2fd58fd002
praneethniu <praneethcbit03@gmail.com>
2018-04-22 17:31:59 -0600

    add redux dev tools

--- src/store.js
+++ src/store.js
@@ -1,11 +1,13 @@
-import { applyMiddleware, createStore, compose } from 'redux';
-import rootReducer from  './reducers';
+import {applyMiddleware, createStore, compose} from 'redux';
+import rootReducer from './reducers';
 import {logger} from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
+import {composeWithDevTools} from 'redux-devtools-extension';
 
-export default(initialState) => {
+
+export default (initialState) => {
     return createStore(
-      rootReducer,
-      applyMiddleware(logger, thunkMiddleware)
+        rootReducer,
+        composeWithDevTools(applyMiddleware(logger, thunkMiddleware))
     );
 }
